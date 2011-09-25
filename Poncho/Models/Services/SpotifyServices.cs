ee3f64685e15c773a9b23e2a5dbe5a9c052f659c
Alexander Asplund <alexasplund@gmail.com>
2011-09-26 00:58:01 +0200

    Started implementing SpotifyServices.Search

--- Poncho/Models/Services/SpotifyServices.cs
+++ Poncho/Models/Services/SpotifyServices.cs
@@ -10,6 +10,7 @@ namespace Poncho.Models.Services
     class SpotifyServices : ISpotifyServices
     {
         private IntPtr _sessionHandle;
+        private IntPtr _searchHandle;
 
         public void FetchPlaylistTracks(PlayList playlist)
         {
@@ -19,12 +20,16 @@ namespace Poncho.Models.Services
         [DllImport("spotify.dll")]
         public static extern void sp_session_login(IntPtr session, ref SByte username, ref SByte password,
                                                    bool rememberMe);
+        [DllImport("spotify.dll")]
+        public static extern void sp_search_create(IntPtr session, ref SByte query, Int32 track_offset, Int32 track_count, Int32 album_offset, Int32 album_count, Int32 artist_offset, Int32 artist_count, ,)
+
+        // sp_search_create(sp_session *session, const char *query, int track_offset, int track_count, int album_offset, 
+        ¨//int album_count, int artist_offset, int artist_count, search_complete_cb *callback, void *userdata);
 
         public void RequestLogin(string username, string password)
         {
             SByte usernameAsSbyte = Convert.ToSByte(username);
             SByte passwordASbyte = Convert.ToSByte(password);
-            _sessionHandle = new IntPtr();
 
             sp_session_login(_sessionHandle, ref usernameAsSbyte, ref passwordASbyte, true);
         }
