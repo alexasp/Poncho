bc8eb645c77940a47622aa8ca1ed730732246749
Alexander Asplund <alexasplund@gmail.com>
2011-09-26 01:44:01 +0200

    Added signatures of C methods to data marshal to be able to complete a search with following callback.

--- Poncho/Models/Services/SpotifyServices.cs
+++ Poncho/Models/Services/SpotifyServices.cs
@@ -5,26 +5,58 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Poncho.Models.services;
 
+
 namespace Poncho.Models.Services
 {
     class SpotifyServices : ISpotifyServices
     {
         private IntPtr _sessionHandle;
         private IntPtr _searchHandle;
-
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void SearchCallbackDelegate(IntPtr searchHandle, IntPtr userdataPointer);
+        
         public void FetchPlaylistTracks(PlayList playlist)
         {
             throw new NotImplementedException();
         }
 
         [DllImport("spotify.dll")]
-        public static extern void sp_session_login(IntPtr session, ref SByte username, ref SByte password,
+        private static extern void sp_session_login(IntPtr session, ref SByte username, ref SByte password,
                                                    bool rememberMe);
-        [DllImport("spotify.dll")]
-        public static extern void sp_search_create(IntPtr session, ref SByte query, Int32 track_offset, Int32 track_count, Int32 album_offset, Int32 album_count, Int32 artist_offset, Int32 artist_count, ,)
 
-        // sp_search_create(sp_session *session, const char *query, int track_offset, int track_count, int album_offset, 
-        ¨//int album_count, int artist_offset, int artist_count, search_complete_cb *callback, void *userdata);
+        [DllImport("spotify.dll")]
+        private static extern void sp_search_create(IntPtr session, ref SByte query, Int32 track_offset,
+                                                    Int32 track_count, Int32 album_offset, Int32 album_count,
+                                                    Int32 artist_offset, Int32 artist_count,
+                                                    SearchCallbackDelegate callbackDelegate, IntPtr userdataPointer);
+
+        //(sp_error) sp_search_error(sp_search *search);
+
+        //(const char *) sp_search_did_you_mean(sp_search *search);
+
+        //(const char *) sp_search_query(sp_search *search);
+
+        //(int) sp_search_total_tracks(sp_search *search);
+
+        //(int) sp_search_num_tracks(sp_search *search);
+
+        //(sp_track *) sp_search_track(sp_search *search, int index);
+
+        //(int) sp_search_num_albums(sp_search *search);
+
+        //(sp_album *) sp_search_album(sp_search *search, int index);
+
+        //(int) sp_search_num_artists(sp_search *search);
+
+        //(sp_artist *) sp_search_artist(sp_search *search, int index);
+
+        //(const char *) sp_artist_name(sp_artist *artist);
+
+        //(const char *) sp_album_name(sp_album *album);
+
+        //(int) sp_album_year(sp_album *album);
+
+        //(const char*) sp_error_message(sp_error error);
 
         public void RequestLogin(string username, string password)
         {
@@ -36,7 +68,14 @@ namespace Poncho.Models.Services
 
         public void Search(string searchText)
         {
-            throw new NotImplementedException();
+            var queryAsSByte = Convert.ToSByte(searchText);
+            var callbackDelegate = new SearchCallbackDelegate(SearchCallback);
+            sp_search_create(_sessionHandle, ref queryAsSByte, 0, 100, 0, 100, 0, 100, callbackDelegate, new IntPtr());
+        }
+
+        private void SearchCallback(IntPtr searchHandle, IntPtr userdataPointer)
+        {
+            //handle searchreturn
         }
     }
 }
