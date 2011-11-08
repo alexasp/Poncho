04e814f3d6e544e02a8fb302186777226d17fd59
Alexander Asplund <alexasplund@gmail.com>
2011-11-08 21:49:17 +0100

    Changed UserFeedbackHandler to interact with a general UserFeedbackDisplay instead of specific ViewModel.

--- SpotifyServiceTests/UserFeedbackHandlerTests.cs
+++ SpotifyServiceTests/UserFeedbackHandlerTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Rhino.Mocks;
+using SpotifyService.Interfaces;
 using SpotifyService.Models;
 using SpotifyService.Models.Enums;
 using SpotifyService.Models.Interfaces;
@@ -10,55 +11,55 @@ namespace SpotifyServiceTests.ModelsTests
     class UserFeedbackHandlerTests
     {
         private UserFeedbackHandler _userFeedbackHandler;
-        private IUserFeedbackViewModel _userFeedbackViewModel;
+        private IUserFeedBackDisplay _userFeedbackDisplay;
 
         [SetUp]
         public void Init()
         {
-            _userFeedbackViewModel = MockRepository.GenerateMock<IUserFeedbackViewModel>();
-            _userFeedbackHandler = new UserFeedbackHandler(_userFeedbackViewModel);
+            _userFeedbackDisplay = MockRepository.GenerateMock<IUserFeedBackDisplay>();
+            _userFeedbackHandler = new UserFeedbackHandler(_userFeedbackDisplay);
         }
 
         [Test]
         public void Display_TrackNotPlayable_SetsUserMessageOnUserFeedbackViewModel()
         {
-            _userFeedbackViewModel.Expect(x => x.DisplayMessage("This track is not playable."));
+            _userFeedbackDisplay.Expect(x => x.DisplayMessage("This track is not playable."));
 
             _userFeedbackHandler.Display(UserFeedback.TrackNotPlayable);
 
-            _userFeedbackViewModel.VerifyAllExpectations();
+            _userFeedbackDisplay.VerifyAllExpectations();
         }
 
         [Test]
         public void Display_TracksNotPlayable_SetsUserMessageOnUserFeedbackViewModel()
         {
-            _userFeedbackViewModel.Expect(x => x.DisplayMessage("One or more of these tracks were not playable."));
+            _userFeedbackDisplay.Expect(x => x.DisplayMessage("One or more of these tracks were not playable."));
 
             _userFeedbackHandler.Display(UserFeedback.SomeTracksNotPlayable);
 
-            _userFeedbackViewModel.VerifyAllExpectations();
+            _userFeedbackDisplay.VerifyAllExpectations();
 
         }
 
         [Test]
         public void Display_NoSearchTextEntered_SetsUserMessageOnUserFeedbackViewModel()
         {
-            _userFeedbackViewModel.Expect(x => x.DisplayMessage("You need to enter a search text."));
+            _userFeedbackDisplay.Expect(x => x.DisplayMessage("You need to enter a search text."));
 
             _userFeedbackHandler.Display(UserFeedback.NoSearchTextEntered);
 
-            _userFeedbackViewModel.VerifyAllExpectations();
+            _userFeedbackDisplay.VerifyAllExpectations();
 
         }
 
         [Test]
         public void Display_InvalidLoginInfo_SersUserMessageOnUserFeedbackViewModel()
         {
-            _userFeedbackViewModel.Expect(x => x.DisplayMessage("The login information you provided was incorrect."));
+            _userFeedbackDisplay.Expect(x => x.DisplayMessage("The login information you provided was incorrect."));
 
             _userFeedbackHandler.Display(UserFeedback.InvalidLoginInfo);
 
-            _userFeedbackViewModel.VerifyAllExpectations();
+            _userFeedbackDisplay.VerifyAllExpectations();
         }
     }
 }
