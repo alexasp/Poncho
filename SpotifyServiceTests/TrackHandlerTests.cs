23b01767b79fecdfacd23e159d77eabf843fcabe
Alexander Asplund <alexasplund@gmail.com>
2011-11-08 13:47:36 +0100

    Final ITVerket sample commit.

---
+++ SpotifyServiceTests/TrackHandlerTests.cs
@@ -0,0 +1,85 @@
+using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SpotifyService.Cargo;
+using SpotifyService.Models;
+using SpotifyService.Models.Interfaces;
+
+namespace SpotifyServiceTests.ModelsTests
+{
+    [TestFixture]
+    class TrackHandlerTests
+    {
+        private IStreamManager _streamManager;
+        private TrackHandler _trackHandler;
+        private ITrackQueue _trackQueue;
+
+        [SetUp]
+        public void Init()
+        {
+            _streamManager = MockRepository.GenerateMock<IStreamManager>();
+            _trackQueue = MockRepository.GenerateMock<ITrackQueue>();
+            _trackHandler = new TrackHandler(_streamManager, _trackQueue);
+        }
+
+        [Test]
+        public void PlayTrack_PlayableTrack_RequestsTrackStreamFromStreamManager()
+        {
+            var track = new Track(true);
+
+            _streamManager.Expect(x => x.RequestTrackStream(track));
+
+            _trackHandler.PlayTrack(track);
+
+            _streamManager.VerifyAllExpectations();
+        }
+
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PlayTrack_TrackNotPlayable_ThrowsInvalidArgumentException()
+        {
+            var track = new Track(false);
+
+            _trackHandler.PlayTrack(track);
+        }
+
+        [Test]
+        public void QueueTrack_TrackPlayable_AddsTrackToQueue()
+        {
+            var track = new Track(true);
+            _trackQueue.Expect(x => x.Enqueue(track));
+
+            _trackHandler.QueueTracks(track);
+
+            _trackQueue.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void HasNextTrack_NextTrackAvailable_ReturnTrue()
+        {
+            _trackQueue.Stub(x => x.Length).Return(1);
+
+            Assert.True(_trackHandler.HasNextTrack());
+        }
+
+        [Test]
+        public void NextTrack_NextTrackAvailable_ReturnsNextTrack()
+        {
+            var track = new Track(true);
+            _trackQueue.Stub(x => x.Dequeue()).Return(track);
+            _trackQueue.Stub(x => x.Length).Return(1);
+
+            Assert.AreEqual(_trackHandler.NextTrack(), track);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [Test]
+        public void NextTrack_NoTrackAvailable_ThrowsInvalidOperationException()
+        {
+            _trackQueue.Stub(x => x.Length).Return(0);
+            _trackHandler.NextTrack();
+        }
+    }
+}
+    
